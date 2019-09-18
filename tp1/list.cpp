d4dd20ea347add5f2942a9dc1a68aef87a5d8c1d
Lucas Augusto <nutkaugusto@gmail.com>
2019-09-18 17:29:08 -0300

    arrumando pastas

---
+++ tp1/list.cpp
@@ -0,0 +1,25 @@
+#include "list.hpp"
+
+FlaskList::FlaskList(){
+
+}
+
+FlaskList::~FlaskList(){
+
+}
+
+void FlaskList::insertFlask(int volume){
+
+}
+
+void FlaskList::removeFlask(int volume){
+
+}
+
+bool FlaskList::haveFlask(int volume){
+
+}
+
+void FlaskList::printList(){
+
+}
