1b4ae389231525bd04293ace5b9e203a3ed6225c
Lucas Augusto <nutkaugusto@gmail.com>
2019-09-24 11:35:51 -0300

    input

--- tp1/main.cpp
+++ tp1/main.cpp
@@ -1,5 +1,21 @@
-#include <iostream>
+#include "list.hpp"
+#include <string>
+
+void inputTreatment(FlaskList &myFlasks, FlaskList &requestedVolumes){
+  int volume;
+  char function;
+  while(!std::cin.eof()){
+    std::cin >> volume;
+    std::cin >> function;
+    // std::cout << volume << " " << function << std::endl;
+
+    std::cin.ignore();
+    fflush(stdin);
+  }
+}
 
 int main(){
-  
+  FlaskList myFlasks;
+  FlaskList requestedVolumes;
+  inputTreatment(myFlasks, requestedVolumes);
 }
