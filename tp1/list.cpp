fd3e64dd625add1d9121bcdda64207257b106ae1
Lucas Augusto <nutkaugusto@gmail.com>
2019-09-23 23:16:15 -0300

    implementaçao da lista

--- tp1/list.cpp
+++ tp1/list.cpp
@@ -1,7 +1,9 @@
 #include "list.hpp"
 
 FlaskList::FlaskList(){
-
+  this->head = nullptr;
+  this->tail = nullptr;
+  this->numberOfFlasks = 0;
 }
 
 FlaskList::~FlaskList(){
@@ -9,17 +11,84 @@ FlaskList::~FlaskList(){
 }
 
 void FlaskList::insertFlask(int volume){
+  Flask *newFlask = new Flask;
+  newFlask->volume = volume;
 
+  if(this->numberOfFlasks == 0){
+    this->head = newFlask;
+    this->tail = newFlask;
+  }else{
+    this->tail->next = newFlask;
+    this->tail = newFlask;
+  }
+  newFlask->next = nullptr;
+  this->numberOfFlasks++;
 }
 
 void FlaskList::removeFlask(int volume){
-
+  Flask *aux = this->head;
+  // primeiro termo
+  if(aux->volume == volume){
+    delete this->head;
+    this->head = aux->next;
+    delete aux;
+    this->numberOfFlasks--;
+  }else{
+    Flask *auxNext = aux->next;
+    // termo do meio
+    while(auxNext != nullptr){
+      if(auxNext->volume == volume){
+        aux->next = auxNext->next;
+        std::cout << "Volume deletado." << std::endl;
+        delete auxNext;
+        delete aux;
+        this->numberOfFlasks--;
+        return;
+      }
+      aux = aux->next;
+      auxNext = auxNext->next;
+    }
+    // ultimo termo
+    if(auxNext->volume == volume){
+      this->tail = aux;
+      std::cout << "Volume deletado." << std::endl;
+      delete auxNext;
+      delete aux;
+      this->numberOfFlasks--;
+      return;
+    }
+    delete auxNext;
+  }
+  delete aux;
+  std::cout << "Este volume não existe." << std::endl;
 }
 
 bool FlaskList::haveFlask(int volume){
-
+  Flask *aux = this->head;
+  int i;
+  for(i=0; i<this->numberOfFlasks; i++){
+    if(aux->volume == volume){
+      std::cout << "O volume de " << volume << " ml existe." << std::endl;
+      delete aux;
+      return true;
+    }
+    aux = aux->next;
+  }
+  delete aux;
+  std::cout << "O volume de " << volume << " ml nao existe." << std::endl;
+  return false;
 }
 
 void FlaskList::printList(){
+  if(this->numberOfFlasks == 0){
+    std::cout << "Lista vazia." << std::endl;
+  }
 
+  Flask *aux = this->head;
+  int i;
+  for(i=0; i<this->numberOfFlasks; i++){
+    std::cout << aux->volume << " ml" << std::endl;  
+    aux = aux->next;
+  }
+  delete aux;
 }
--- tp1/list.hpp
+++ tp1/list.hpp
@@ -1,10 +1,11 @@
+#include <iostream>
 #ifndef LIST_H
 #define LIST_H
 
 struct Flask{
   int volume;
   Flask *next;
-}
+};
 
 class FlaskList{
   private:
@@ -18,5 +19,5 @@ class FlaskList{
     void removeFlask(int volume);
     bool haveFlask(int volume);
     void printList();
-}
+};
 #endif
