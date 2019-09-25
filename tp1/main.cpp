25099ab813a01333079c5a30d2f527b4bee3f11c
Lucas Augusto <nutkaugusto@gmail.com>
2019-09-24 21:29:26 -0300

    arrumando input

--- tp1/main.cpp
+++ tp1/main.cpp
@@ -4,10 +4,17 @@
 void inputTreatment(FlaskList &myFlasks, FlaskList &requestedVolumes){
   int volume;
   char function;
-  while(!std::cin.eof()){
-    std::cin >> volume;
-    std::cin >> function;
-    // std::cout << volume << " " << function << std::endl;
+
+  while(std::cin >> volume >> function){
+    if(function == 'i'){
+      myFlasks.insertFlask(volume);
+      std::cout << "insere " << volume << std::endl;
+    }else if(function == 'r'){
+      myFlasks.removeFlask(volume);
+      std::cout << "remove " << volume << std::endl;
+    }else if(function == 'p'){
+      myFlasks.printList();
+    }
 
     std::cin.ignore();
     fflush(stdin);
